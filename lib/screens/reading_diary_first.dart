8e61e43cf233d8b3a4b128175492afae798c9740
BrenoFischer <breno_perricone@hotmail.com>
2020-10-27 23:19:01 -0300

    setup inicial firebase

--- lib/screens/reading_diary_first.dart
+++ lib/screens/reading_diary_first.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:travelog/classes/diary.dart';
-import 'package:travelog/components/show_diary_map.dart';
 import 'package:travelog/screens/reading_page.dart';
 import 'package:travelog/components/round_button.dart';
 
