ef2a09a2a9b05316dfad2177e25b66649c35b7a7
BrenoFischer <breno_perricone@hotmail.com>
2020-11-19 21:23:55 -0300

    ajustar usuário persistente já logado

--- lib/root.dart
+++ lib/root.dart
@@ -3,17 +3,24 @@ import 'package:get/get.dart';
 import 'package:travelog/auth_controller.dart';
 import 'package:travelog/screens/welcome.dart';
 import 'package:travelog/screens/login.dart';
+import 'package:travelog/services/database.dart';
 import 'package:travelog/user_controller.dart';
 
 class Root extends GetWidget<AuthController> {
   @override
   Widget build(BuildContext context) {
+    void handleRoot() async {
+      Get.find<UserController>().user =
+          await Database().getUser(Get.find<AuthController>().user?.uid);
+    }
+
     return GetX(
       initState: (_) async {
         Get.put<UserController>(UserController());
       },
       builder: (_) {
         if (Get.find<AuthController>().user?.uid != null) {
+          handleRoot();
           return WelcomeScreen();
         } else {
           return Login();
